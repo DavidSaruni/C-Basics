ea41480c0cb4bebc9bb6d61b820b124ff3a0b432
DavidSaruni <sarunidavid11126@gmail.com>
2024-02-16 18:56:34 +0300

    Learning C# Basics

---
+++ helloworld/Program.cs
@@ -0,0 +1,50 @@
+﻿
+namespace ClassObject {
+
+  class Employee {
+
+    string department;
+
+    //methods
+    public string getDepartment() {
+      return department;
+    }
+
+    //employee can take leave
+    public void takeLeave() {
+      Console.WriteLine("Employee is on leave");
+    }
+
+    //employee can work
+    public void work() {
+      Console.WriteLine("Employee is at work");
+    }
+
+    //employee can take breakfast
+    public void takeBreakfast() {
+      Console.WriteLine("Employee is taking breakfast");
+    }
+
+    static void Main(string[] args) {
+
+      // create Employee object 
+      Employee sheeran = new Employee();
+
+      // set department for sheeran
+      sheeran.department = "Development";
+      Console.WriteLine("Sheeran: " + sheeran.department);
+
+      // create second object of Employee
+      Employee taylor = new Employee();
+
+      // set department for taylor
+      taylor.department = "Content Writing";
+      Console.WriteLine("Taylor: " + taylor.department);
+
+        // call methods
+        sheeran.takeLeave();
+
+      Console.ReadLine();
+    }
+  }
+}
